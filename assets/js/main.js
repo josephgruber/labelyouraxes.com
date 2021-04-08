a3f7ad049084ada7d20cc825e02c7cb74d8387f0
Joseph Gruber <joseph.gruber@gmail.com>
2021-04-08 13:26:25 -0600

    Updated font style and colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 $(function () {
+    Chart.defaults.font.size = 16;
+    Chart.defaults.font.weight = 500;
     var ctx = document.getElementById("chart");
     var myLineChart = new Chart(ctx, {
         type: "line",
@@ -8,13 +10,13 @@ $(function () {
                 {
                     data: [65, 59, 80, 81, 56, 55, 40],
                     fill: false,
-                    borderColor: "rgb(75, 192, 192)",
+                    borderColor: "rgb(117, 163, 58)",
                     tension: 0.1,
                 },
                 {
                     data: [83, 96, 43, 21, 36, 95, 39],
                     fill: false,
-                    borderColor: "rgb(75, 192, 192)",
+                    borderColor: "rgb(244, 193, 142)",
                     tension: 0.1,
                 },
             ],
@@ -23,6 +25,11 @@ $(function () {
             plugins: {
                 legend: {
                     display: false,
+                },
+            },
+            scales: {
+                y: {
+                    color: "black",
                 },
             },
         },
